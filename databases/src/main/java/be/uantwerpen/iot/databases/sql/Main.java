e7e5ad4c5219155966f99886f1643a276a4f3c7d
Thomas Cassimon <thomas.cassimon@outlook.com>
2018-10-18 17:26:29 +0200

    Added Music class. Added JPA dependency.

--- databases/src/main/java/be/uantwerpen/iot/databases/sql/Main.java
+++ databases/src/main/java/be/uantwerpen/iot/databases/sql/Main.java
@@ -2,4 +2,8 @@ package java.be.uantwerpen.iot.databases.sql;
 
 public class Main
 {
+    public static void main (String[] args)
+    {
+
+    }
 }
---
+++ databases/src/main/java/be/uantwerpen/iot/databases/sql/model/Music.java
@@ -0,0 +1,30 @@
+package java.be.uantwerpen.iot.databases.sql.model;
+
+public class Music
+{
+    private String title;
+    private String artist;
+    private int year;
+
+    public Music(String title, String artist, int year)
+    {
+        this.title = title;
+        this.artist = artist;
+        this.year = year;
+    }
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public String getArtist()
+    {
+        return this.artist;
+    }
+
+    public int getYear()
+    {
+        return this.year;
+    }
+}
