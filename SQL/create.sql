2f572aefc61cc59c9b6067679b5992d2afbcb57d
ThomasCassimon <thomas.cassimon@outlook.com>
2018-10-19 15:47:02 +0200

    Further expansion of the README

--- SQL/create.sql
+++ SQL/create.sql
@@ -1,7 +1,7 @@
 /*	The music database contains only a single table, the Music table	*/
 
 /*	Music Table	*/
-CREATE TABLE MUSIC (_ID		INTEGER 		PRIMARY KEY NOT NULL AUTO_INCREMENT,	/*	Song ID								*/
+CREATE TABLE MUSIC (_ID		BIGINT	 		PRIMARY KEY NOT NULL AUTO_INCREMENT,	/*	Song ID								*/
 					TITLE	VARCHAR(255)	NOT NULL,								/*	Song Title	(Max. 255 Characters)	*/
 					ARTIST	VARCHAR(255)	NOT NULL,								/*	Artist(s)	(Max. 255 Characters)	*/
 					YEAR	INTEGER			NOT NULL);								/*	Year		32-bit signed integer	*/
