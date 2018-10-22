f81c14286392e427f7623492281ff417a5b207b7
Astrid <astrid.vanneste1@gmail.com>
2018-10-22 17:35:15 +0200

    setup script noSQL

---
+++ noSQL/create.js
@@ -0,0 +1,45 @@
+connection = new Mongo()
+
+db = connection.getDB("Votes")
+
+db.createCollection("Votes"
+{
+    validator:
+    {
+        bsonType:"object",
+        required: [timestamp, value, username, uid, songid],
+        properties:
+        {
+            timestamp:
+            {
+                bsonType: "string",
+                description: "Time when the vote was made"
+            },
+
+            value
+            {
+                bsonType: "int",
+                description: "+1 for upvote, -1 for downvote"
+            },
+
+            username
+            {
+                bsonType: "string",
+                description: "username of the user"
+            },
+
+            uid
+            {
+                bsonType: "string",
+                description: "id of the user"
+            },
+
+            songid
+            {
+                bsonType: "int",
+                description: "id of the song"
+            }
+        }
+    }
+
+})
